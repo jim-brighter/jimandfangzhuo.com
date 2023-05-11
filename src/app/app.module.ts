8fba9c9687bd873cd6df6ed838181048a3862f8b
Jim Brighter <jim-brighter@users.noreply.github.com>
2023-05-10 22:08:13 -0400

    angular upgrade (#98)
    * angular upgrade
    
    * dependencies update
    
    * background update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { CommentsComponent } from './comments/comments.component';
 import { ListsComponent } from './lists/lists.component';
 import { DetailsComponent } from './details/details.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EventService } from './event.service';
@@ -18,7 +18,7 @@ import { ListItemComponent } from './list-item/list-item.component';
 import { CommentService } from './comment.service';
 import { CommentItemComponent } from './comment-item/comment-item.component';
 import { UploadComponent } from './upload/upload.component';
-import { FileUploadModule } from 'ng2-file-upload';
+// import { FileUploadModule } from 'ng2-file-upload';
 import { ImageService } from './image.service';
 import { PhotosComponent } from './photos/photos.component';
 import { LoginComponent } from './login/login.component';
@@ -58,7 +58,6 @@ export class XhrInterceptor implements HttpInterceptor {
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    FileUploadModule,
     FontAwesomeModule,
     AmplifyAuthenticatorModule
   ],
