4d7237d0113b15cbb2387e9e89d8ef9e23cb6ae3
Jim Brighter <jbrighter92@gmail.com>
2023-06-19 11:42:01 -0400

    comment cleanup

--- ui/src/app/services/authentication.service.ts
+++ ui/src/app/services/authentication.service.ts
@@ -15,10 +15,6 @@ export class AuthenticationService {
   authenticated = false;
   idToken: string = '';
 
-  // authToken: string;
-  // csrfCookie: string;
-  // tokenExpiration: string;
-
   constructor(private http: HttpClient,
               private errors: ErrorService) {
     const tokenAuth = localStorage.getItem(TOKEN_KEY);
@@ -26,23 +22,10 @@ export class AuthenticationService {
     const tokenExpiration = localStorage.getItem(TOKEN_EXPIRATION) || '';
     if (tokenAuth && tokenCsrf && (new Date().getTime() < parseInt(tokenExpiration))) {
       this.authenticated = true;
-      // this.authToken = tokenAuth;
-      // this.csrfCookie = tokenCsrf;
-      // this.tokenExpiration = tokenExpiration;
     } else {
       this.wipeSession();
     }
   }
-
-  // private getCsrfCookie(): string {
-  //   const tab = document.cookie.split(';');
-  //   if (tab.length > 0) {
-  //     const tab1 = tab[0].split('=');
-  //     if (tab1.length > 1) {
-  //       return tab1[1];
-  //     }
-  //   }
-  // }
 
   async authenticate(credentials: {username: string, password: string}, callback: () => void) {
     try {
@@ -53,36 +36,6 @@ export class AuthenticationService {
     } catch(e) {
       console.error(e);
     }
-
-    // const headers = new HttpHeaders(credentials ? {
-    //   authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
-    // } : {});
-
-    // this.authenticated = true;
-
-    // this.http.get(this.rootAuthUrl + '/token', { headers: headers, withCredentials: true })
-    //   .pipe(
-    //     catchError(this.handleError('authenticate', null))
-    //   )
-    //   .subscribe(response => {
-    //     if (response && response['token']) {
-    //       this.authenticated = true;
-
-    //       this.authToken = response['token'];
-    //       localStorage.setItem(TOKEN_KEY, this.authToken);
-
-    //       this.csrfCookie = this.getCsrfCookie();
-    //       localStorage.setItem(CSRF_KEY, this.csrfCookie);
-
-    //       this.tokenExpiration = response['token_expiration'];
-    //       localStorage.setItem(TOKEN_EXPIRATION, this.tokenExpiration);
-
-    //       this.errors.clear();
-    //     } else {
-    //       this.wipeSession();
-    //     }
-    //
-    //   });
   }
 
   async logout() {
@@ -94,20 +47,6 @@ export class AuthenticationService {
     } catch(e) {
       console.error(e);
     }
-    // this.http.post(this.rootAuthUrl + `/logout?_csrf=${this.csrfCookie}`, {}, {
-    //   responseType: 'text',
-    //   headers: new HttpHeaders({
-    //     'X-Auth-Token': this.authToken,
-    //     'X-Xsrf-Token': this.csrfCookie
-    //   }),
-    //   withCredentials: true
-    // })
-    // .pipe(
-    //   catchError(this.handleError('logout', null))
-    // )
-    // .subscribe(response => {
-    //   this.wipeSession();
-    // });
   }
 
   private handleError<T> (operation = 'operation', result ?: T) {
@@ -118,9 +57,6 @@ export class AuthenticationService {
   private wipeSession() {
     this.authenticated = false;
     this.idToken = '';
-    // this.authToken = null;
-    // this.csrfCookie = null;
-    // this.tokenExpiration = null;
     localStorage.clear();
   }
 
