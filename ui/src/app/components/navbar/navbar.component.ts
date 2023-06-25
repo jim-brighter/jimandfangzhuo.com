179613081e20b1a2ab2a4297169e4f70adde4e1b
Jim Brighter <jim-brighter@users.noreply.github.com>
2023-06-25 15:00:57 -0400

    move logout component into navbar (#161)

--- ui/src/app/components/navbar/navbar.component.ts
+++ ui/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
+import { KeyValue } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +13,34 @@ export class NavbarComponent implements OnInit {
 
   @Input() linkColor: string = '';
 
-  constructor() { }
+  faSignOutAlt = faSignOutAlt;
+
+  routerMapping = {
+    "/details/to-do": "Doing!",
+    "/details/to-eat": "Eating!",
+    "/details/to-cook": "Cooking!",
+    "/details/to-go": "Going!",
+    "/details/to-watch": "Watching!",
+    "/details/to-buy": "Buying!",
+    "/photos": "Photos!"
+  }
+
+  constructor(private authenticator: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onCompare(_left: KeyValue<any, any>, _right: KeyValue<any, any>): number {
+    return 1;
+  }
+
+  showLogout(): boolean {
+    return this.router.url !== '/';
+  }
+
+  logout(): void {
+    this.authenticator.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
