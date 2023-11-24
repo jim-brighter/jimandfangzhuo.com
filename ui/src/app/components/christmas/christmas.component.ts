dca789e8f1ac57618341191dc3960ecd18b32d80
Jim Brighter <jbrighter92@gmail.com>
2023-11-24 15:59:35 -0500

    add items

--- ui/src/app/components/christmas/christmas.component.ts
+++ ui/src/app/components/christmas/christmas.component.ts
@@ -10,6 +10,27 @@ export class ChristmasComponent {
 
     navigationSubscription;
 
+    data = [
+        {
+            name: 'Harry Potter 3 Minalima book',
+            link: 'https://www.amazon.com/Harry-Potter-Prisoner-Azkaban-MinaLima/dp/1338815288'
+        },
+        {
+            name: 'Set of new plates'
+        },
+        {
+            name: 'Pajamas - Fangzhuo (regular small)',
+            link: 'https://www.llbean.com/llb/shop/118952?page=womens-1912-flannel-pajamas-plaid-misses-regular&bc=12-27-613&feat=613-GN1&csp=f&attrValue_0=Black%20Watch&pos=1'
+        },
+        {
+            name: 'Portable washing machine',
+            link: 'https://www.amazon.com/dp/B0CD7DKXLF?ref_=cm_sw_r_apin_dp_8VMNWW8AV09ENVKYQRY5&language=en-US&th=1'
+        },
+        {
+            name: 'Gift cards - Costco, Target, Amazon'
+        }
+    ]
+
     constructor(private router: Router) {
         this.navigationSubscription = this.router.events.subscribe(async (e: any) => {
 
