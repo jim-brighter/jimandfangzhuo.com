2e68d50bd471db915c3fca8c015aec39ce68ef35
Jim Brighter <jbrighter92@gmail.com>
2024-11-26 00:06:06 -0500

    step 3

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,10 +1,22 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/modules/app.module';
+import { XhrInterceptor } from './app/modules/app.module';
 import { environment } from './environments/environment';
 
 import { Amplify } from 'aws-amplify';
+import { EventService } from './app/services/event.service';
+import { CommentService } from './app/services/comment.service';
+import { ImageService } from './app/services/image.service';
+import { AuthenticationService } from './app/services/authentication.service';
+import { ErrorService } from './app/services/error.service';
+import { ChristmasService } from './app/services/christmas.service';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/modules/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { AppComponent } from './app/components/app/app.component';
 
 Amplify.configure({
   Auth: {
@@ -19,5 +31,17 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule, AmplifyAuthenticatorModule),
+        EventService,
+        CommentService,
+        ImageService,
+        AuthenticationService,
+        ErrorService,
+        ChristmasService,
+        { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
