1f7f522edd8ca557cd5e85215d888821526972a1
Jim Brighter <jim-brighter@users.noreply.github.com>
2026-04-09 22:08:33 -0400

    turn of replication for initial deployment (#651)

--- cdk/lib/backend/ImageS3Bucket.ts
+++ cdk/lib/backend/ImageS3Bucket.ts
@@ -20,11 +20,11 @@ export class ImageS3Bucket extends Bucket {
         expiredObjectDeleteMarker: true,
         noncurrentVersionExpiration: Duration.days(30)
       }],
-      replicationRules: [{
-        deleteMarkerReplication: true,
-        destination: Bucket.fromBucketName(scope, replicationId, replicationBucketName),
-        priority: 1
-      }]
+      // replicationRules: [{
+      //   deleteMarkerReplication: true,
+      //   destination: Bucket.fromBucketName(scope, replicationId, replicationBucketName),
+      //   priority: 1
+      // }]
     });
   }
 }
