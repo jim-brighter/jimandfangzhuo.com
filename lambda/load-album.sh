10db324f8b4faf7f3835cea8e70222c50ad2dac2
Jim Brighter <jim-brighter@users.noreply.github.com>
2026-05-12 23:35:46 -0400

    resize lambda function (#681)

--- lambda/load-album.sh
+++ lambda/load-album.sh
@@ -9,6 +9,14 @@ function doSync() {
   local albumId=$(uuidgen)
   local createdAt=$(date +%s)
 
+  echo "Album Name: $albumName"
+
+  read -r -p "Sync \"$albumName\" to S3? [y/N] " confirm
+  if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+    echo "Aborted."
+    exit 0
+  fi
+
   aws s3 sync "$fullPath" "s3://jimandfangzhuo.com-images-us-east-1/$albumName"
 
   aws dynamodb put-item \
